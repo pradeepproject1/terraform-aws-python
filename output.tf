1d6f2179095e5354580a536a65a74e1428e62e25
pradeepproject1 <pradeepkonda85@gmail.com>
2026-08-01 20:41:06 +0530

    -m added yaml

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
-output "instance_id" {
-  value = aws_instance.demo.id
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
 }
 
-output "instance_public_ip" {
-  value = aws_instance.demo.public_ip
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
 }
 
-output "start_url" {
-  value = "${aws_apigatewayv2_stage.default.invoke_url}/start"
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
 }
 
-output "stop_url" {
-  value = "${aws_apigatewayv2_stage.default.invoke_url}/stop"
+output "kubeconfig_command" {
+  value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.main.name}"
 }
